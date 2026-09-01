d434bd7d62ac1d48d9777fba86e990268732aa8d
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-01 10:17:33 +0300

    Wire app theme mode to settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
+import 'core/theme/theme_controller.dart';
 import 'mockups/login/login_screen.dart';
 
 void main() {
@@ -11,13 +12,16 @@ class YbbMockupApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'YBB UX Mockup',
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.dark,
-      home: const LoginScreen(),
+    return AnimatedBuilder(
+      animation: themeController,
+      builder: (context, _) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'YBB UX Mockup',
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        themeMode: themeController.mode,
+        home: const LoginScreen(),
+      ),
     );
   }
 }
